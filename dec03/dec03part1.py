6b7faa952ad8fa1ddcd5a31f0ada6e8138136e8f
Sarah Withee <github@sarahwithee.com>
2016-12-02 23:35:17 -0600

    Add 12/3 solutions

--- dec03/dec03part1.py
+++ dec03/dec03part1.py
@@ -2,3 +2,33 @@
 # Dec 3, Part 1
 # @geekygirlsarah
 
+inputFile = "input.txt"
+
+# Tracking vars
+possibleTriangles = 0
+
+with open(inputFile) as f:
+    while True:
+        line = f.readline(-1)
+        if not line:
+            # print "End of file"
+            break
+        # print ("Line: ", line)
+
+        # print ("First number=" + str(lastNumber))
+
+        numsList = line.split(" ")
+        nums = []
+        for num in numsList:
+            print("Num: " + str(num))
+            if num.rstrip().isdigit():
+                nums.append(int(num))
+
+        # print(nums)
+        if (nums[0] + nums[1] > nums[2] and
+            nums[1] + nums[2] > nums[0] and
+            nums[0] + nums[2] > nums[1]):
+            possibleTriangles = possibleTriangles + 1
+
+
+print ("Number of possible triangles: " + str(possibleTriangles))
--- dec03/dec03part2.py
+++ dec03/dec03part2.py
@@ -2,3 +2,35 @@
 # Dec 3, Part 2
 # @geekygirlsarah
 
+inputFile = "input.txt"
+
+# Tracking vars
+possibleTriangles = 0
+allNums = []
+
+with open(inputFile) as f:
+    while True:
+        line = f.readline(-1)
+        if not line:
+            # print "End of file"
+            break
+        # print ("Line: ", line)
+
+        numsList = line.split(" ")
+        nums = []
+        for num in numsList:
+            # print("Num: " + str(num))
+            if num.rstrip().isdigit():
+                nums.append(int(num))
+        allNums.append(nums)
+
+# print(nums)
+while len(allNums) > 2:
+    for i in range(0, 3):
+        # print (str(i))
+        print ("Triangle " + str(i) + ": " + str(allNums[0][i]) + " " + str(allNums[1][i]) + " " + str(allNums[2][i]))
+        if (allNums[0][i] + allNums[1][i] > allNums[2][i] and allNums[1][i] + allNums[2][i] > allNums[0][i] and allNums[0][i] + allNums[2][i] > allNums[1][i]):
+            possibleTriangles = possibleTriangles + 1
+    del allNums[0:3]
+
+print ("Number of possible triangles: " + str(possibleTriangles))
