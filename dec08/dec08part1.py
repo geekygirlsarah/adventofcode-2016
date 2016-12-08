ab05bd9efa97395b0da186a983e2694186a7c75c
Sarah Withee <github@sarahwithee.com>
2016-12-07 22:58:34 -0600

    Add 12/8 files

---
+++ dec08/dec08part1.py
@@ -0,0 +1,18 @@
+# Advent of Code
+# Dec 8, Part 1
+# @geekygirlsarah
+
+inputFile = "input.txt"
+
+# Tracking vars
+
+
+# Look at each row, add it's value to each alphabet tracker
+with open(inputFile) as f:
+    while True:
+        line = f.readline(-1)
+        if not line:
+            break
+
+
+
---
+++ dec08/dec08part2.py
@@ -0,0 +1,4 @@
+# Advent of Code
+# Dec 8, Part 2
+# @geekygirlsarah
+
